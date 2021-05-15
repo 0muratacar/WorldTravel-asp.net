f4f6ebdfb28a64481e81b11a91182af04e2fe570
murat <macar18@marun.edu.tr>
2021-05-16 02:54:23 +0300

    Proje dosyası ekle.

---
+++ Models/UserDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldTravel.Models
+{
+    public class UserDataModel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Eposta { get; set; }
+        public string Telefon { get; set; }
+
+        public AddresModel Address { get; set; }
+    }
+}
---
+++ Pages/navbarPages/Sign.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WorldTravel.Models;
+
+namespace WorldTravel.Pages.navbarPages
+{
+    public class SignModel : PageModel
+    {
+
+
+        [BindProperty]
+        public UserDataModel UserData { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("/Index" ,new {Name=UserData.Ad, Surname=UserData.Soyad });
+        }
+    }
+}
